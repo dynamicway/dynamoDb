1681a8cd6932496421e25226e3c63dc123a1a4a0
bee <rkddbtjd123@gmail.com>
2024-07-01 18:32:47 +0900

    feat: Add EntryDynamoDbTable

---
+++ src/main/java/me/study/dynamodb/event/infrastructure/EntryDynamoDbTable.java
@@ -0,0 +1,44 @@
+package me.study.dynamodb.event.infrastructure;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import me.study.dynamodb.event.domain.EventPrize;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@DynamoDbBean
+public class EntryDynamoDbTable {
+    private String partitionKey;
+    private String sortKey;
+    private long userId;
+    private EventPrize prize;
+
+    public EntryDynamoDbTable(long userId, EventPrize prize) {
+        this.userId = userId;
+        this.prize = prize;
+    }
+
+    @DynamoDbPartitionKey
+    @DynamoDbAttribute("pk")
+    public String getPartitionKey() {
+        return "u#" + userId;
+    }
+
+    @DynamoDbSortKey
+    @DynamoDbAttribute("sk")
+    public String getSortKey() {
+        return "entry";
+    }
+
+    @DynamoDbAttribute("prize")
+    public EventPrize getPrize() {
+        return prize;
+    }
+
+}
